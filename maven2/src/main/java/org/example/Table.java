878a444f89cdec8f376ab53494d3511f50db3bca
Mehedi Hasan <mehedi.developer@icloud.com>
2024-03-11 10:03:08 -0600

    init

---
+++ maven2/src/main/java/org/example/Table.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Table {
+    public static void main(String[] args) {
+        int tableno = 2;
+        int increament = 1;
+        System.out.println(tableno+ " * "+ increament +" = "+ + tableno*increament);
+        increament = 2;
+        System.out.println(tableno+ " * "+ increament +" = "+ + tableno*increament);
+    }
+}
+
+
+
+
+
+
+
+
+
