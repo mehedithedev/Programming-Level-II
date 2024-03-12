6a7bd96b399c618d83ad7d29ac8459f179b4da48
Mehedi Hasan <mehedi.developer@icloud.com>
2024-03-12 13:40:58 -0600

    init

--- maven2/src/main/java/org/example/Input.java
+++ maven2/src/main/java/org/example/Input.java
@@ -1,2 +1,16 @@
-package org.example;public class Input {
+package org.example;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Input {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(2);
+        System.out.println(c);
+        System.out.println("The data type of value c is: "+ ((Object)c).getClass().getSimpleName());
+    }
+
 }
--- maven2/src/main/java/org/example/userInput.java
+++ maven2/src/main/java/org/example/userInput.java
@@ -1,2 +1,76 @@
-package org.example;public class userInput {
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * @author kasha
+ */
+
+public class userInput {
+
+    public static void main(String[] args) {
+
+
+        int tableno = 5;
+
+        int loop1 = 0;
+
+
+        System.out.println("input the table number: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        tableno = sc.nextInt();
+
+
+        System.out.println("input the start number: ");
+
+        sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+
+
+        System.out.println("input the stop number: ");
+
+        sc = new Scanner(System.in);
+
+        int stop = sc.nextInt();
+
+
+        System.out.println("Please enter 'a' for acsending or 'd' for descending : ");
+
+        char c = sc.next().charAt(0);
+
+
+        if (c == 'd') {
+
+            loop1 = start;
+
+
+            do {
+
+
+                System.out.println(tableno + " * " + (loop1) + " = " + tableno * (loop1));
+
+                loop1--;
+
+            }
+
+            while (loop1 > stop - 1);
+
+
+        } else if (c == 'a') {
+            loop1 = stop;
+
+            do {
+                System.out.println(tableno + " * " + (loop1) + " = " + tableno * (loop1));
+
+                loop1++;
+            } while (loop1 < start + 1);
+
+
+        }
+
+    }
+
 }
